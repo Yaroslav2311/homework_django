831da673f509d984a4771f3c6eeac26aa8dc8e4c
Yaroslav Boiko <yaroslav.boyko.2311@gmail.com>
2022-10-19 15:45:34 +0300

    flake8_example

--- myproject/settings.py
+++ myproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-tahf^l&(*004kwa9au#k_01$dq2#*b_=4te7(w!#*(k5^k8ha2'
+# SECRET_KEY = 'django-insecure-tahf^l&(*004kwa9au#k_01$dq2#*b_=4te7(w!#*(k5^k8ha2'
 SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-tahf^l&(*004kwa9au#k_01$dq2#*b_=4te7(w!#*(k5^k8ha2')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
