29fca43e771aca2790d497aa3ea62f533f70c91f
Yaroslav Boiko <yaroslav.boyko.2311@gmail.com>
2022-11-01 16:30:27 +0200

    hw9

--- catalog/forms.py
+++ catalog/forms.py
@@ -3,7 +3,6 @@ from django import forms
 
 
 class Triangle(forms.Form):
-    template_name = 'catalog/triangle.html'
     leg1 = forms.IntegerField(label='enter the length of the first leg', min_value=1)
     leg2 = forms.IntegerField(label='enter the length of the second leg', min_value=1)
 
