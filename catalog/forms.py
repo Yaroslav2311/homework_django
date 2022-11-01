dad8d1d8da4d04a72227f90c7bd622339a390295
Yaroslav Boiko <yaroslav.boyko.2311@gmail.com>
2022-11-01 16:31:55 +0200

    hw9

--- catalog/forms.py
+++ catalog/forms.py
@@ -5,8 +5,3 @@ from django import forms
 class Triangle(forms.Form):
     leg1 = forms.IntegerField(label='enter the length of the first leg', min_value=1)
     leg2 = forms.IntegerField(label='enter the length of the second leg', min_value=1)
-
-# class Person(forms.Form):
-#     first_name = forms.CharField(required=True, max_length=150)
-#     last_name = forms.CharField(required=True, max_length=150)
-#     email = forms.EmailField(required=True)
